fb75e1d037970103cfe65f2edb99872fccebebe5
Ratul Bhuiyan <143863186+RatulInnovates@users.noreply.github.com>
2025-07-22 10:51:58 +0600

    Add files via upload

---
+++ sth.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"HEll0 adfdsaf"<<endl;
+    int a,b, sum;
+    cin>>a>>b;
+    sum = a+b;
+    cout<<sum;
+
+
+
+
+
+    return 0;
+}
